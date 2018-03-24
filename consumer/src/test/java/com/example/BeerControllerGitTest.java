58f961f1d0cefd3afcbfe9e79aa12025d5016200
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-03-25 00:38:52 +0100

    Added samples for Git Downloader

---
+++ consumer/src/test/java/com/example/BeerControllerGitTest.java
@@ -0,0 +1,112 @@
+package com.example;
+
+import org.assertj.core.api.BDDAssertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+//remove::start[]
+// example of usage with fixed port
+//@AutoConfigureStubRunner(workOffline = true, ids = "com.example:beer-api-producer:+:stubs:8090")
+@AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.REMOTE,
+		repositoryRoot = "git://https://github.com/marcingrzejszczak/contract-git",
+		ids = { "com.example:beer-api-producer-external:0.0.1-SNAPSHOT", "foo.bar:bazService:0.0.1-SNAPSHOT"})
+//remove::end[]
+@DirtiesContext
+//@org.junit.Ignore
+public class BeerControllerGitTest extends AbstractTest {
+
+	@Autowired MockMvc mockMvc;
+	@Autowired BeerController beerController;
+
+	//remove::start[]
+	@StubRunnerPort("beer-api-producer-external") int producerPort;
+	@StubRunnerPort("bazService") int bazServicePort;
+
+	@Before
+	public void setupPort() {
+		beerController.port = producerPort;
+	}
+	//remove::end[]
+	//tag::tests[]
+	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
+		//remove::start[]
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 22)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("THERE YOU GO"));
+		//remove::end[]
+	}
+
+	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
+		//remove::start[]
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 17)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("GET LOST"));
+		//remove::end[]
+	}
+
+	@Test public void should_work_for_another_service_from_git() throws Exception {
+		//remove::start[]
+		ResponseEntity<String> entity = new RestTemplate()
+				.getForEntity("http://localhost:" + this.bazServicePort + "/hello",
+						String.class);
+
+		BDDAssertions.then(entity.getStatusCodeValue()).isEqualTo(200);
+		//remove::end[]
+	}
+	//end::tests[]
+}
+
+
+//remove::start[]
+/*
+
+
+
+	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 22)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("THERE YOU GO"));
+	}
+
+	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 17)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("GET LOST"));
+	}
+
+ */
+//remove::end[]
